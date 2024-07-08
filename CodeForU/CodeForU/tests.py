f5aef308a15b7814817ac62f0f094097a019650a
Mohamad-Aj <117522649+Mohamad-Aj@users.noreply.github.com>
2024-07-08 12:56:07 +0300

    test the jenkins

--- CodeForU/CodeForU/tests.py
+++ CodeForU/CodeForU/tests.py
@@ -4,3 +4,8 @@ class BasicMathTestCase(TestCase):
     def test_addition(self):
         """Test addition of two numbers"""
         self.assertEqual(2 + 2, 4)
+
+class BasicMathTestCase2(TestCase):
+    def test_addition(self):
+        """Test addition of two numbers"""
+        self.assertEqual(3 + 2, 5)
