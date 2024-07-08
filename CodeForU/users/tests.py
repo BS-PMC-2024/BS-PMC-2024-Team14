b88a971d4c9c84b6cbcd56d0153b02aeb7ff10c2
Mohamad-Aj <117522649+Mohamad-Aj@users.noreply.github.com>
2024-07-08 12:50:25 +0300

    Added docker file and jenkins

--- CodeForU/users/tests.py
+++ CodeForU/users/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class BasicMathTestCase(TestCase):
+    def test_addition(self):
+        """Test addition of two numbers"""
+        self.assertEqual(2 + 2, 4)
