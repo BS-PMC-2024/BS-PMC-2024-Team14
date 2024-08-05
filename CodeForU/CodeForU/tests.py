52b67566aba3c8b1acb85047fb0381b10691d00a
Lidorfel <109869934+Lidorfel@users.noreply.github.com>
2024-08-05 17:51:17 +0300

    Update tests.py again

--- CodeForU/CodeForU/tests.py
+++ CodeForU/CodeForU/tests.py
@@ -1,19 +1,19 @@
-from django.test import TestCase
-import unittest
+#from django.test import TestCase
+#import unittest
 
 # Import the test cases from the users app
-from users.tests import UserModelTests
+#from users.tests import UserModelTests
 
 # Create a test suite that includes the user tests
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(UserModelTests))
-    return suite
+#def suite():
+#    suite = unittest.TestSuite()
+#    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(UserModelTests))
+#    return suite
 
 # Create a test case to run the suite
-class MainAppTestSuite(TestCase):
+#class MainAppTestSuite(TestCase):
 
-    def test_users_app(self):
-        runner = unittest.TextTestRunner()
-        result = runner.run(suite())
-        self.assertEqual(result.wasSuccessful(), True)
+#    def test_users_app(self):
+#        runner = unittest.TextTestRunner()
+#        result = runner.run(suite())
+#       self.assertEqual(result.wasSuccessful(), True)
