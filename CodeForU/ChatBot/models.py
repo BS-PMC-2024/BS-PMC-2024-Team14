40899b7fc9b6260b1a80ba28b2815b47b551e954
Mohamad-Aj <mohamadaj310@gmail.com>
2024-08-05 17:17:38 +0300

    BSPMS2414-33-- finished question related functions to admin

--- CodeForU/ChatBot/models.py
+++ CodeForU/ChatBot/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.conf import settings
 
 class Question(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.IntegerField(blank=True,null=True)
     question_text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Question by {self.user.email} at {self.created_at}"
+        return f"Question by user ID {self.user} at {self.created_at}"
 
--- CodeForU/users/admin.py
+++ CodeForU/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, Student, Mentor, Question
+from .models import User, Student, Mentor
 
 admin.site.register(User)
 admin.site.register(Student)
 admin.site.register(Mentor)
-admin.site.register(Question)
